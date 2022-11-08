ae95ed962a921fd5329ee04731d13e2f6d63af3c
AlvaroP <alvaro05pomog@gmail.com>
2022-11-08 08:36:31 +0100

    replenish

--- UsefulActions/UsefulAcions.sh
+++ UsefulActions/UsefulAcions.sh
@@ -5,32 +5,44 @@ action=$2
 
 rc=0
 
+if [ $# -le 1 ]; then
+
+        exit 1
+
+fi
+
 userList=$(cat /etc/passwd | cut -d ":" -f1 | grep $user) || rc=1
+
 
 if [ $rc -eq 1 ]; then
 
 	echo "User given not exist"
-
+	action=0
 fi
 
 if [ $action = "replenish" ]; then
 
 
 	mkdir /home/Workspace
-	touch sample.txt
 
-	mkdir config
-	touch sample.txt
+	mkdir /home/Workspace/config
+	touch /home/Workspace/config/sample.txt
 
-	mkdir bin
-	touch sample.txt
+	mkdir /home/Workspace/bin
+	touch /home/Workspace/bin/sample.txt
 
-	mkdir source
+	mkdir /home/Workspace/source
+	touch /home/Workspace/source/sample.txt
 
-	mkdir rsrc
 
-	
+	mkdir /home/Workspace/rsrc
+	touch /home/Workspace/rsrc/sample.txt
+	touch /home/Workspace/sample.txt
+
 
 fi
+
+
+exit 0
 
 
