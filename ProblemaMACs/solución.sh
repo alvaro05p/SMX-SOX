647cda741cc18d7aee8f178f5e0b6f3a4ad446b8
AlvaroP <alvaro05pomog@gmail.com>
2022-11-25 10:25:19 +0100

    organizando

--- ProblemaMACs/solución.sh
+++ ProblemaMACs/solución.sh
@@ -1,85 +1,71 @@
 #!/bin/bash
 
+#Comprobamos si la ip de este eqipo cosigue hacer ping
+
 while true ; do
 
-ip=$(ip a | grep metric | cut -d " " -f6 | cut -d "/" -f1)
+	ip=$(ip a | grep metric | cut -d " " -f6 | cut -d "/" -f1)
 
-sleep 5
+	sleep 1
 
-control=$(ping -c 1 $ip | grep ", 0% packet loss" | wc -l)
+	control=$(ping -c 1 10.0.2.3 | grep ", 0% packet loss" | wc -l)
 
 
-if [ $control = 1 ]; then
+	if [ $control = 1 ]; then
 
         echo "Funka"
 
+#Si hubiera un erro en el ping mirariamos en el fichero arp.log
 
-elif [ $control = 0 ]; then
+	elif [ $control = 0 ]; then
 
-        if [ -f arp.log ]; then
-
-
-                echo "Actualizando tu fichero arp ya que se encontró un error"
-
-                rm arp.log
-
-                wget http://tic.ieslasenia.org/arp.log
-
-        else
-
-                wget http://tic.ieslasenia.org/arp.log
-
-                echo "Se ha descargado el fichero arp por que se encontro un error"
+        	if [ -f arp.log ]; then
 
 
-                        echo $lineas
+                	echo "Actualizando tu fichero arp ya que se encontró un error"
+
+                	rm arp.log
+
+                	wget http://tic.ieslasenia.org/arp.log
+
+        	else
+
+        		wget http://tic.ieslasenia.org/arp.log
+
+                	echo "Se ha descargado el fichero arp por que se encontro un error"
 
 
+		fi
 
 
+		while true; do
 
-        fi
+			sleep 1
 
-
-
-
-
-if [ -f textinfo ]; then
-
-        echo "Todo good"
-
-else
-
-        touch textinfo
-
-fi
-
-while true; do
-
-sleep 1
-
-          cat arp.log | cut -d " " -f5 | while read lineas ; do
+			cat arp.log | cut -d " " -f5 | while read lineas ; do
 
 
-        echo "hola" > textinfo
+				echo $lineas > textinfo
 
-        info=$(cat textinfo)
+        			info=$(cat textinfo)
 
 
 
-                if [ $palabra = $info ]; then
+                		if [ $palabra = $info ]; then
 
 
-                        echo $textinfo
+                        		echo "Error en las MAC"
 
-                fi
+                		fi
 
-        done
+			done
 
-        done
+        	done
+
+
+	fi
 
 done
-
 
 
 exit 0
