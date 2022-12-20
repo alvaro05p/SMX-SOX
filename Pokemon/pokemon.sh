1d2031d0a8e5cbc330d79ce64f77a9c9870a7623
alvaro05p <alvaro05pomg@gmail.com>
2022-12-20 17:57:18 +0100

    Pokemon perfecto

--- Pokemon/pokemon.sh
+++ Pokemon/pokemon.sh
@@ -7,21 +7,28 @@ defensa=$2
 
 contador=0
 
+#Un bucle que lo recorra de arriba a bajo
 cat data.txt | sed -n 2,19p | cut -d "," -f1 | while read line; do
 
+#Contador aprovechando que los tipos de pokemon estan en el mismo orden tanto en ataque como defensa
 let contador=contador+1
 
-echo $contador
-echo $line
 
-atacantes=$(cat data.txt | sed 2,19p | grep -i $ataque)
+if [ $line = $defensa ]; then
 
-echo $atacantes
+#Buscamos el numero en que se encuentra hacia bajo y este coincidirá con el horizontal
+
+
+	let contador=contador+1
+
+#Mostramos solo el campo que queremos y lo mostramos
+	puntos=$(cat data.txt | sed -n 2,19p | grep -i $ataque | cut -d "," -f$contador)
+	echo "Tu multiplicador fueron $puntos puntos"
+
+fi
+
 
 done
-
-
-
 
 
 exit 0
