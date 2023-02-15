1696fe70651c81d42f582dcc3090d1cd69eca4f8
alvaro05p <117514691+alvaro05p@users.noreply.github.com>
2023-02-15 11:53:33 +0100

    Add files via upload

---
+++ scriptldap.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+
+user=$1
+
+groupo=$(ldapsearch -x -b dc=ubuntusrv,dc=smx2023,dc=net -H ldap://192.168.5.123>
+
+
+if [ $group = "trasgos" ]; then
+
+mount 192.168.5.158:/srv/nfs/exam-ldap-nfs /home/alvaro05p/shared/smx2023.net>
+
+fi
+
+exit 0
